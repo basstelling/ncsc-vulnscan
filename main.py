a10be8ce2d7c8e2c16b8dda66197d861abc682e4
basstelling <b.stelling@st.hanze.nl>
2022-05-08 16:08:19 +0200

    finished headerScan.py, added service detection per port to portScan.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from portScan import portScan
+from headerScan import headerChecker
 
-ip = input("Voer het te scannen IP adres in: ")
-portScan(ip)
+resp = input("Welkom bij deze tool. Voer het nummer in van de actie die u wilt uitvoeren. \n\n"
+            "1) Portscan\n"
+            "2) Web Header Configuration\n")
+
+if resp == '1':
+    host = input("Voer het te scannen IP adres in:\n")
+    ports = input("Voer de te scannen poort(en) in:\n")
+    portScan(host, ports)
+
+if resp == '2':
+    url = input("Voer de URL in waarvan u de headers wilt analyseren: \n")
+    headerChecker(url)
+
+    # headerChecker('https://vulnerable-website.com')
+    # headerChecker('https://www.python.org')
 
--- portScan.py
+++ portScan.py
@@ -1,35 +1,41 @@
-from multiprocessing.sharedctypes import Value
+from unicodedata import name
+import socket
 import nmap
-import ipaddress
-from datetime import datetime
+import time
 
-def portScan(target):
+def portScan(target,ports):
     # portScanner object aanmaken
-    scanner = nmap.PortScanner()
-    # scannen van hosts op bepaalde poorten volgens bepaalde nmap flags
-    scanner.scan(target, ports='22-443', arguments='-T4')
-    hostlist = scanner.all_hosts()
-    startTime = datetime.now()
+    try:
+        scanner = nmap.PortScanner()
+        startTime = time.time()
+        # scannen van hosts op bepaalde poorten volgens bepaalde nmap flags
+        scanner.scan(target, ports=ports, arguments='-T4')
+        hostlist = scanner.all_hosts()
+        # door alle gescande hosts loopen
+        for host in hostlist:
+            try:
+                # per host het ip en hostname ophalen en printen waar mogelijk
+                print('-----------------------')
+                print('Host : %s (%s)' % (host, scanner[host].hostname()))
+                for protocol in scanner[host].all_protocols():
+                    print('-----')
+                    print('Protocol : %s' % protocol)
 
-    # door alle gescande hosts loopen
-    for host in hostlist:
-        try:
-            # per host ip + host name printen waar mogelijk
-            print('-----------------------')
-            print('Host : %s (%s)' % (host, scanner[host].hostname()))
-            for protocol in scanner[host].all_protocols():
-                print('-----')
-                print('Protocol: %s' % protocol)
+                    portList = sorted(scanner[host][protocol])
+                    for port in portList:
+                            print('port : %s\tstatus : %s\t service : %s' % (port, scanner[host][protocol][port]['state'], scanner[host][protocol][port]['name']))
+                    print('\n')
+            except KeyError as e:
+                print(e)
+                return False
+    except nmap.PortScannerError as b:
+        print("Er is geen installatie van Nmap gevonden in de omgevingsvariabelen van deze machine.\n", b)
+        return False
 
-                portList = sorted(scanner[host][protocol])
-                for port in portList:
-                    print('port: %s\tstate : %s' % (port, scanner[host][protocol][port]['state']))
-        except KeyError as e:
-            print(e)
-            return False
+    # totale tijd teruggeven
+    totalTime = time.time() - startTime
+    print('-----------------------')
+    print('De scan heeft', format(totalTime, ".4f"), 'seconden geduurd.')
+    print('-----------------------')
 
-        # tijd teruggeven
-        totalTime = datetime.now() - startTime
-        print('-----------------------')
-        print('De scan heeft', totalTime, 'minuten geduurd.')
-        print('-----------------------')
+# portScan('192.168.1.1/26', '22-23')
